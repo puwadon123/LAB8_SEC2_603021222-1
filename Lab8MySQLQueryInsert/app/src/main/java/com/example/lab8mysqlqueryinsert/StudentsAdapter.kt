1cb51fab93dd41d696d78deef6256ade7f597e6b
Supawitkongkum <53934250+Supawitkongkum@users.noreply.github.com>
2019-10-05 18:10:11 +0700

    LAB8_SEC2_603021222-1
    LAB8_SEC2_603021222-1

---
+++ Lab8MySQLQueryInsert/app/src/main/java/com/example/lab8mysqlqueryinsert/StudentsAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.lab8mysqlqueryinsert
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.std_item_layout.view.*
+
+class StudentsAdapter(val item: List<Student>, val context: Context) : RecyclerView.Adapter<StudentsAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view_item = LayoutInflater.from(parent.context).inflate(R.layout.std_item_layout,parent,false)
+        return ViewHolder(view_item)
+    }
+
+    override fun getItemCount(): Int {
+        return item.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.tvID?.text =  holder.tvID.text.toString()+item[position].std_id
+        holder.tvName?.text = item[position].std_name
+        holder.tvAge?.text = holder.tvAge.text.toString()+item[position].std_age.toString()
+
+    }
+
+    class ViewHolder (view:View) : RecyclerView.ViewHolder(view){
+        val tvID : TextView = view.tv_id
+        val tvName : TextView = view.tv_name
+        val tvAge : TextView = view.tv_age
+    }
+
+}
